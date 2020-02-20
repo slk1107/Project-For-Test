80e18b05ecc89387f75f63700aad3865b1cd8dbb
Kris <>
2020-02-20 22:54:18 +0800

    Create Project

---
+++ Project for Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project for Test
+//
+//  Created by Kris on 2020/2/20.
+//  Copyright © 2020 Kris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Project for TestTests/Project_for_TestTests.swift
@@ -0,0 +1,34 @@
+//
+//  Project_for_TestTests.swift
+//  Project for TestTests
+//
+//  Created by Kris on 2020/2/20.
+//  Copyright © 2020 Kris. All rights reserved.
+//
+
+import XCTest
+@testable import Project_for_Test
+
+class Project_for_TestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
